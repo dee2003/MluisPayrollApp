794c7bed7beea7f8e1866f9177da11ad62a220e2
Manisha K <manisha.rk04@gmail.com>
2025-10-15 10:31:40 +0530

    WIP: local changes before merge

--- backend/models.py
+++ backend/models.py
@@ -39,10 +39,10 @@ class Ticket(Base):
     extracted_text = Column(String, index=True, nullable=False)
     # This line is the crucial addition
     image_path = Column(String, nullable=True)  # Path to the saved image file
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    #owner_id = Column(Integer, ForeignKey("users.id"))
     created_at = Column(DateTime, default=func.now())
 
-    owner = relationship("User", back_populates="tickets")
+    #owner = relationship("User", back_populates="tickets")
     foreman_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
 
     # Relationships
